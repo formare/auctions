7c2f6ef9d38212cf78c451da492cd8ab466d1435
Christoph Lange <math.semantic.web@gmail.com>
2013-02-23 13:22:06 +0000

    restructured

--- isabelle/Auction/Questions.thy
+++ isabelle/Auction/Questions.thy
@@ -2,42 +2,60 @@ theory Questions
 imports Main
 begin
 
-(* TODO CL: ask whether there a way of writing the alternative as b_hat, as it looks in the paper version? *)
-(* makarius: Isabelle is text based, and Isabelle/jEdit is a plain text editor, but there are
-   many tricks that can be played with "visual tuning" of formal documents; most of this works
-   for the generated LaTeX.  Here is just a cheap one:
+(* This file includes some questions, mainly related to usability, that had occurred during the formalisation, and their answers. *)
 
-  We re-use an existing letter symbol from the Isabelle repertoire.  Its LaTeX interpretation
-  is re-defined in document/root.tex like this:
+(*** Soft-wrapping long lines ***)
 
-   \renewcommand{\isasymbb}{\isamath{\hat b}}
+(* Christoph: when setting long lines to wrap at the window boundary in Isabelle/jEdit 2012, the wrapped part behaved badly and wasn't even editable. *)
 
-  You can also try to find alternative Unicode mapping for $ISABELLE_HOME_USER/etc/symbols
-  but it rarely works out so well in practice.
- *)
+    (* Makarius: soft-wrap is rarely used; in Isabelle2012 its rendering was indeed broken; should work in Isabelle2013 *)
+
+        (* Christoph: We've had this discussion before on the isabelle-users mailing list.  I still disagree to some extent.  In the interest of comprehensibility I preferred long, descriptive symbol names here, which inevitably make lines long.  In the wide-screen age, you wouldn't want to hard-wrap all of them to, say, 80 columns.  But we might agree on something like 120. *)
+
+(* Christoph: Does jEdit's Isabelle mode disable word wrapping? *)
+
+    (* Makarius: depending what word wrapping means exactly, it might be a conflict of tokens of jEdit
+    vs. tokens of Isabelle; note that Isabelle/jEdit installs its own token marker for approximative
+    syntax highlighting; *)
+
+        (* Christoph: I had actually been referring to soft line wrapping here. *)
+
+(*** Hat accents on top of mathematical symbol names ***)
+
+(* Christoph: Is there a way of writing the alternative bid as \hat{b}, as it is done in the LaTeX paper? *)
+
+    (* Makarius: Isabelle is text based, and Isabelle/jEdit is a plain text editor, but there are many tricks that can be played with "visual tuning" of formal documents; most of this works for the generated LaTeX.  Here is just a cheap one:
+
+    We re-use an existing letter symbol from the Isabelle repertoire.  Its LaTeX interpretation
+    is re-defined in document/root.tex like this:
+
+    \renewcommand{\isasymbb}{\isamath{\hat b}}
+
+    You can also try to find alternative Unicode mapping for $ISABELLE_HOME_USER/etc/symbols
+    but it rarely works out so well in practice. *)
 
 definition "\<bb> = 0"
 
+(*** Some auto-completions cause syntax errors. ***)
 
-(* TODO CL: report jEdit bug that suggested completions for nat (\<nat>) and bool (\<bool>) cause syntax errors *)
-(* makarius: completion still lacks context (Isabelle2013), so it can propose things that don't make sense *)
+(* Christoph: jEdit's suggested completions for nat (\<nat>) and bool (\<bool>) cause Isabelle syntax errors. *)
 
-(* TODO CL: report Isabelle/jEdit bug: no auto-indentation *)
-(* makarius: not a bug, but lack of feature (when it eventually arrives, people will complain
-   about being forced into Isar standard indentation) *)
+    (* Makarius: completion still lacks context (Isabelle2013), so it can propose things that don't make sense *)
 
-(* TODO CL: report Isabelle/jEdit bug: can't copy goal in output pane to clipboard *)
-(* makarius: this should work in Isabelle2012 already, using plain CONTROL-C, not menus;
-   in Isabelle2013 it is even more robust, since the Output panel uses the same technology as
-   jEdit editor buffers (still no menu operation, though) *)
+(*** No auto-indentation ***)
 
-(* TODO CL: report Isabelle/jEdit bug: when I set long lines to wrap at window boundary, wrapped part behaves badly: not editable *)
-(* makarius: soft-wrap is rarely used; in Isabelle2012 its rendering was indeed broken;
-   should work in Isabelle2013 *)
+(* Christoph: Isabelle/jEdit does not support auto-indentation. *)
 
-(* TODO CL: ask whether jEdit's Isabelle mode disables word wrapping *)
-(* makarius: depending what word wrapping means exactly, it might be a conflict of tokens of jEdit
-   vs. tokens of Isabelle; note that Isabelle/jEdit installs its own token marker for approximative
-   syntax highlighting; *)
+    (* Makarius: This is not a bug, but a lack of feature (when it eventually arrives, people will complain about being forced into Isar standard indentation) *) 
+
+(*** Can't copy goal in output pane to clipboard ***)
+
+(* Christoph: probably a bug in Isabelle2012 *)
+
+    (* Makarius: this should work in Isabelle2012 already, using plain CONTROL-C, not menus;
+    in Isabelle2013 it is even more robust, since the Output panel uses the same technology as
+    jEdit editor buffers (still no menu operation, though) *)
+
+        (* Christoph: I never use menus for this, but I wouldn't want to install Isabelle2012 again just to verify _that_ ;-) *)
 
 end
