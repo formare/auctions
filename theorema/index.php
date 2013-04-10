ae849663ab9688c60e1043eb54921025dea38515
Christoph Lange <math.semantic.web@gmail.com>
2013-04-10 00:10:27 +0000

    more information

--- theorema/index.php
+++ theorema/index.php
@@ -1,12 +1,14 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/sys/includes/wwwlib.php'); 
+$formare_root = '../../../';
+require_once($formare_root . 'macros.php');
 
 $title = 'Auction Theory Toolbox (ATT) – Theorema';
 
 SetHeaderTitle($title);
 SetPageTitle($title);
 SetMaintainer('Christoph Lange', 'c.lange@cs.bham.ac.uk');
-AddStylesheet('../../../formare.css');
+AddStylesheet($formare_root . 'formare.css');
 
 HTMLHeader();
 PageStart();
@@ -20,9 +22,11 @@ PageStart();
 <h2>How to run</h2>
 <ol>
   <li>Get a licence of <a href="http://www.wolfram.com/mathematica/">Mathematica 8</a>.</li>
-  <li>For now, please contact <a href="http://www.risc.jku.at/people/wwindste/">Wolfgang Windsteiger</a> for a copy of Theorema.</li>
-  <li>…</li>
+  <li>For now, please contact <a href="http://www.risc.jku.at/people/wwindste/">Wolfgang Windsteiger</a> for a copy of Theorema and for installation instructions.</li>
+  <li>Open the Theorema formalisation in Mathematica.</li>
+  <li>Evaluate the cell that reads <code>Needs["Theorema`"]</code> by placing the cursor there and pressing Shift+Return, or evaluate all initialization cells via <em>Evaluation→Evaluate Initialization Cells</em>.</li>
 </ol>
+<p style="text-align:right; font-style:italic"><? echo $timestamp ?></p>
 <?php
 PageEnd();
 HTMLFooter();
