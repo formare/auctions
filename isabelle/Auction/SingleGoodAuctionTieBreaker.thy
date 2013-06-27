d2ff268f880befce0f79e8f42b4b11c8afa4d058
Christoph Lange <math.semantic.web@gmail.com>
2013-06-27 00:27:10 +0000

    adding three more theories factored out of Vickrey_CaseChecker.thy

---
+++ isabelle/Auction/SingleGoodAuctionTieBreaker.thy
@@ -0,0 +1,51 @@
+(*
+$Id: SingleGoodAuction.thy 507 2013-02-23 14:59:19Z langec $
+
+Auction Theory Toolbox
+
+Authors:
+* Manfred Kerber <m.kerber@cs.bham.ac.uk>
+* Christoph Lange <math.semantic.web@gmail.com>
+* Colin Rowat <c.rowat@bham.ac.uk>
+* Makarius Wenzel <wenzel@lri.fr>
+
+Dually licenced under
+* Creative Commons Attribution (CC-BY) 3.0
+* ISC License (1-clause BSD License)
+See LICENSE file for details
+(Rationale for this dual licence: http://arxiv.org/abs/1107.3212)
+*)
+
+header {* Fully specified single good auctions (with tie-breaking) *}
+
+theory SingleGoodAuctionTieBreaker
+(* TODO CL: This is actually about single _indivisible_ goods; consider renaming theory *)
+imports Main
+  "~~/src/HOL/Library/Order_Relation"
+  SingleGoodAuction
+begin
+
+
+text{* Helps to determine whether one participant should be preferred over another one in the case of a draw.
+  t x y = True should result in x being (strictly) preferred. *}
+type_synonym tie_breaker = "participant \<Rightarrow> participant \<Rightarrow> bool"
+
+text{* Is a tie-breaker well-behaved on a given set of participants?  I.e. is it a strict
+  linear order? *}
+definition wb_tie_breaker_on :: "tie_breaker \<Rightarrow> participant set \<Rightarrow> bool"
+  where "wb_tie_breaker_on t N \<longleftrightarrow>
+    strict_linear_order_on N
+      {(a::participant, b::participant) . {a, b} \<subseteq> N \<and> t a b}"
+(* old version for tie_breaker = "participant \<Rightarrow> real", to be used with <
+   "card (tie_breaker ` N) = card N" *)
+
+(* CL: code provided by Le_J (http://stackoverflow.com/a/16690357/2397768) –
+   how to prove strict linear order in a concrete case: *)
+(*
+lemma "strict_linear_order_on {1::nat, 2} {(a::nat, b) . {a, b} \<subseteq> {1::nat, 2} \<and> a < b}"
+  unfolding strict_linear_order_on_def partial_order_on_def preorder_on_def
+    irrefl_def total_on_def trans_def antisym_def
+  by auto
+*)
+
+end
