74b17fd7392e246ba86d207590784c5661f9c707
Christoph Lange <math.semantic.web@gmail.com>
2013-06-28 15:48:14 +0000

    integrated relational and functional implementations of potential_buyer side-by-side

--- isabelle/Auction/nVCG_CaseChecker.thy
+++ isabelle/Auction/nVCG_CaseChecker.thy
@@ -23,7 +23,7 @@ header {* Vickrey's Theorem: second price auctions are
 
 theory nVCG_CaseChecker
 imports Complex_Main
-  "~~/src/HOL/Library/Function_Algebras"
+  (* "~~/src/HOL/Library/Function_Algebras" (* was needed when we added vectors component-wise *) *)
   "~~/src/HOL/Library/Order_Relation"
   "~~/src/HOL/Library/Efficient_Nat"
   Vectors
@@ -33,10 +33,10 @@ imports Complex_Main
 begin
 
 (*
-can assume or not assume free disposal (bid(A u B) >= max{ bid(A), bid(B) })
+We can assume or not assume (as it suits us) free disposal, i.e. bid A \<union> B \<ge> max (bid A) (bid B).
 
-for now assume that the value to the seller is 0
-free to assume or not to assume Sum xn <= x0 or Sum xn = x0
+For now assume that the value of the goods to the seller is 0.
+We are free to assume or not to assume (as it suits us) \<Sum> n \<in> N . x n \<le> x 0, or \<Sum> n \<in> N . x n = x 0.
 *)
 
 section {* Combinatorial auctions *}
@@ -45,59 +45,23 @@ subsection {* Preliminaries *}
 
 type_synonym participant = index
 type_synonym goods = "nat set" (* actually we'd prefer "'a set", as we really don't care about the type *)
-type_synonym price = nat (* or maybe real, later *)
+type_synonym price = nat (* or maybe "real", later *)
 
 (*
+Keeping old initial vector-based bid implementation for reference.
+
 (* one participant's bid on a set of goods *)
 definition b :: "participant \<Rightarrow> goods \<Rightarrow> nat" where "b i y = (\<Sum> x \<in> y . x)"
-
-(* for one set of goods in one particular partition of the overall set of goods, \<dots> *)
-definition f :: "goods \<Rightarrow> participant" where "f Y = (if Y = {} then 1 else 0)"
-
-(* *)
-definition h :participant: "(goods \<Rightarrow> participant) \<Rightarrow> goods \<Rightarrow> nat" where "h F y = b (F y) y"
-
-value "\<lambda> Y f . \<Sum> y \<in> Y . h f y"
-*)
-
-(*
-value "Max ((\<lambda> Yf::((goods set) \<times> (goods \<Rightarrow> participant)) . let Y = fst Yf; f = snd Yf in \<Sum> y \<in> Y . h f y) ` {(Y, f) . True})"
-*)
-
-(* definition h (f, b) = % y . *)
-
-notepad
-begin
-  def participants \<equiv> "{1::nat, 2}"
-  def goods \<equiv> "{3::nat, 4}"
-  def bids \<equiv> "\<lambda> (y::nat set) . 51::nat"
-  (*
-  value "\<Sum> x \<in> {{3::nat},{4}} . h x"
-  *)
-end
-
-(* equivalence classes and partitions *)
-
-(* CL: maybe uncomment and use as an alternative representation
 
 text{* convenience type synonyms for most of the basic concepts we are dealing with *}
 type_synonym endowment = "nat vector" (* conceptually: good \<Rightarrow> quantity *)
 type_synonym endowment_subset = "nat vector" (* conceptually the same, but \<le> endowment *)
-*)
 
 (* A single participant ascribes real values to subsets of the endowment. *)
-(*
 type_synonym valuation = "endowment_subset \<Rightarrow> real"
-
 type_synonym valuations = "valuation vector"
-*)
-
-(*
 type_synonym bid = "endowment_subset \<Rightarrow> real"
-(* endowment = (1,2)
-   bid = { (0,1) \<rightarrow> 3.45, (1,2) \<rightarrow> 7.42, \<dots> } *)
-*)
-(*
+
 (* A well-formed bid is non-negative for each “subset” of the endowment, i.e. each vector s
    that is component-wise 0 \<le> s \<le> x0. *)
 definition bid :: "bid \<Rightarrow> goods \<Rightarrow> endowment \<Rightarrow> bool"
@@ -105,128 +69,109 @@ definition bid :: "bid \<Rightarrow> goods \<Rightarrow> endowment \<Rightarrow>
 
 type_synonym allocation = "participant \<Rightarrow> endowment_subset"
 
-(* x0 = (1,2) 
-   x1 = (1,1)
-   x2 = (0,1) <- K
-    ^
-    N
-*)
-
-value "(\<lambda>z::nat . ((\<lambda>x::nat.2*x) z + (\<lambda>x::nat.1) z)) 3::nat"
-value "((\<lambda>x::nat.2*x) + (\<lambda>x::nat.1)) 3::nat"
-
-definition allocation :: "participants \<Rightarrow> goods \<Rightarrow> allocation \<Rightarrow> bool"
-  where "allocation N K x \<longleftrightarrow> 
-    (\<Sum> i \<in> N . x i)
-
-non_negative_real_vector N x \<and> (\<Sum> i \<in> N . x i) = 1"
-
-
 type_synonym payments = "real vector"
 *)
-(* MC: Some tries *)
-
 type_synonym bids = "participant \<Rightarrow> goods \<Rightarrow> price"
-type_synonym allocation = "(goods set) \<times> ((goods \<times> participant) set)"
+type_synonym allocation_fun = "(goods set) \<times> (goods \<Rightarrow> participant)"
+type_synonym allocation_rel = "(goods set) \<times> ((goods \<times> participant) set)"
+type_synonym tie_breaker_rel = "(allocation_rel set) \<Rightarrow> allocation_rel"
+type_synonym tie_breaker_fun = "(allocation_fun set) \<Rightarrow> allocation_fun"
 
 (* a particular example for bids: *)
-definition bb :: "bids"
+definition bb :: bids
 where "bb x y = x"
 
 (* a particular example for the "potential buyer" relation: *)
 definition ff :: "(goods \<times> participant) set"
 where "ff = {(g,p) . p = 1}"
 
-definition F :: "bids => allocation => price"
-where "F b Yp  = (let Y = fst Yp; potential_buyer = snd Yp in
+(* the revenue gained from selling a certain allocation (assuming relational allocations) *)
+definition revenue_rel :: "bids \<Rightarrow> allocation_rel \<Rightarrow> price"
+where "revenue_rel b Yp  = (let Y = fst Yp; potential_buyer = snd Yp in
   \<Sum> y \<in> Y . b (as_function potential_buyer y) y)"
 
-(* TODO CL: for the computational version, reimplement potential_buyer as a relation
-   Then it's easy to show injectivity, to compute the inverse, etc. *)
-definition possible_allocations :: "goods => participant set  => allocation set"
-where "possible_allocations G N = { (Y,potential_buyer) .
+(* the revenue gained from selling a certain allocation (assuming functional allocations) *)
+definition revenue_fun :: "bids \<Rightarrow> allocation_fun \<Rightarrow> price"
+where "revenue_fun b Yp  = (let Y = fst Yp; potential_buyer = snd Yp in
+  \<Sum> y \<in> Y . b (potential_buyer y) y)"
+
+(* the set of possible allocations of a set of goods to a set of participants (assuming relational allocations) *)
+definition possible_allocations_rel :: "goods \<Rightarrow> participant set \<Rightarrow> allocation_rel set"
+where "possible_allocations_rel G N = { (Y,potential_buyer) .
   Y \<in> allPartitions G
   \<and> Domain potential_buyer = Y
-  \<and> function_on potential_buyer Y
   \<and> Range potential_buyer \<subseteq> N
+  \<and> function_on potential_buyer Y
   \<and> injective potential_buyer
  }"
 
+(* the set of possible allocations of a set of goods to a set of participants (assuming functional allocations) *)
+definition possible_allocations_fun :: "goods \<Rightarrow> participant set \<Rightarrow> allocation_fun set"
+where "possible_allocations_fun G N = { (Y,potential_buyer) .
+  Y \<in> allPartitions G
+  \<and> (\<forall> y \<in> Y . potential_buyer y \<in> N)
+  \<and> inj_on potential_buyer Y
+ }"
+
+(* the maximum revenue over all possible allocations (assuming relational allocations) *)
 definition max_revenue :: "bids \<Rightarrow> goods \<Rightarrow> participant set \<Rightarrow> price"
-where "max_revenue b G N = Max ((F b) ` (possible_allocations G N))"
+where "max_revenue b G N = Max ((revenue_rel b) ` (possible_allocations_rel G N))"
+(* we don't need the variant that assumes functional allocations, as it's really just the same *)
 
-definition winning_allocations (* "arg max" *) :: "bids \<Rightarrow> goods \<Rightarrow> participant set \<Rightarrow> (allocation set)"
-where "winning_allocations b G N = 
-{ (Y,potential_buyer) . F b (Y,potential_buyer) = max_revenue b G N}"
+(* This is the "arg max", where max_revenue is the "max" (assuming relational allocations). *)
+definition winning_allocations_rel :: "bids \<Rightarrow> goods \<Rightarrow> participant set \<Rightarrow> (allocation_rel set)"
+where "winning_allocations_rel b G N = 
+{ (Y,potential_buyer) . revenue_rel b (Y,potential_buyer) = max_revenue b G N}"
 
-type_synonym tie_breaker = "(allocation set) \<Rightarrow> allocation"
+(* This is the "arg max", where max_revenue is the "max" (assuming functional allocations). *)
+definition winning_allocations_fun :: "bids \<Rightarrow> goods \<Rightarrow> participant set \<Rightarrow> (allocation_fun set)"
+where "winning_allocations_fun b G N = 
+{ (Y,potential_buyer) . revenue_fun b (Y,potential_buyer) = max_revenue b G N}"
 
-definition tt::tie_breaker where "tt x = (THE y . (y \<in> x))"
+(* example: break ties by preferring an arbitrary allocation (assuming relational allocations) *)
+definition tt_rel :: tie_breaker_rel where "tt_rel x = (THE y . y \<in> x)"
 
-definition NN::"participant set" where "NN=\<nat>"
-definition GG::"goods" where "GG=\<nat>"
+(* example: break ties by preferring an arbitrary allocation (assuming functional allocations) *)
+definition tt_fun :: tie_breaker_fun where "tt_fun x = (THE y . y \<in> x)"
 
+(* CL@MC: not sure \<nat> does what we want; and in any case our participants and goods sets are assumed to be finite. *)
+definition NN :: "participant set" where "NN = \<nat>"
+definition GG :: goods where "GG = \<nat>"
 
-definition winning_allocation::"tie_breaker => bids \<Rightarrow> goods \<Rightarrow> participant set \<Rightarrow> allocation"
-where "winning_allocation t b G N  = t (winning_allocations b G N)"
+(* the unique winning allocation that remains after tie-breaking (assuming relational allocations) *)
+definition winning_allocation_rel :: "tie_breaker_rel => bids \<Rightarrow> goods \<Rightarrow> participant set \<Rightarrow> allocation_rel"
+where "winning_allocation_rel t b G N  = t (winning_allocations_rel b G N)"
 
-definition alpha where "alpha b G N n = max_revenue b G (N - {n})" 
+(* the unique winning allocation that remains after tie-breaking (assuming functional allocations) *)
+definition winning_allocation_fun :: "tie_breaker_fun => bids \<Rightarrow> goods \<Rightarrow> participant set \<Rightarrow> allocation_fun"
+where "winning_allocation_fun t b G N  = t (winning_allocations_fun b G N)"
 
-definition w2g::"tie_breaker => bids => goods => (participant set) => participant => goods" 
-where "w2g t b G N = inv (snd (winning_allocation t b G N))"
+(* the value reportedly generated by value maximization problem when solved without n's bids *)
+definition \<alpha> :: "bids \<Rightarrow> goods \<Rightarrow> participant set \<Rightarrow> participant \<Rightarrow> price"
+where "\<alpha> b G N n = max_revenue b G (N - {n})"
+
+definition w2g :: "tie_breaker_fun => bids => goods => (participant set) => participant => goods" 
+where "w2g t b G N = inv (snd (winning_allocation_fun t b G N))"
+
 definition foo where "foo t b G N n = b n (w2g t b G N n)" (* won_set *)
 definition bar where "bar t b G N = (%n . foo t b G N n)"
-definition ss
-::"tie_breaker => bids => goods => (participant set) => participant => price"
-where 
-"ss t b G N n = setsum (bar t b G N) (N-{n})"
+definition ss :: "tie_breaker_fun => bids => goods => (participant set) => participant => price"
+where "ss t b G N n = setsum (bar t b G N) (N-{n})"
 
-definition prices::"tie_breaker => bids => goods => (participant set) => (participant => price)"
-where "prices t b G N = (alpha b G N) - (ss t b G N)"
+definition payments_fun :: "tie_breaker_fun => bids => goods => (participant set) => (participant => price)"
+where "payments_fun t b G N = \<alpha> b G N - ss t b G N"
 
-end
-
-definition alpha :: "bids \<Rightarrow> goods \<Rightarrow> participant set \<Rightarrow> participant \<Rightarrow> price"
-where "alpha b G N n = max_revenue b G (N - {n})"
-(* \<dots> = Max ((F b) ` (possible_allocations G (N - {n})))
-      = Max ((F b) ` { (Y,potential_buyer) . 
-                       Y \<in> allPartitions G
-                       \<and> (\<forall> x::goods . x \<in> Y \<longrightarrow> potential_buyer x \<in> (N - {n}))
-                       \<and> inj_on potential_buyer Y })
-      = Max ({ F b (Y,potential_buyer) .
-                Y \<in> allPartitions G
-                \<and> (\<forall> x::goods . x \<in> Y \<longrightarrow> potential_buyer x \<in> (N - {n}))
-                \<and> inj_on potential_buyer Y })
-      = Max ({ (\<Sum> y \<in> Y . b (potential_buyer y) y) .
-                Y \<in> allPartitions G
-                \<and> (\<forall> x::goods . x \<in> Y \<longrightarrow> potential_buyer x \<in> (N - {n}))
-                \<and> inj_on potential_buyer Y })
- *)
-
-(*
-definition foo :: "bids => goods => participant set => tie_breaker => (goods => participant)" where "foo b G N t = snd (t (winning_allocations b G N))"
-(* TODO CL: get this well-typed with t::tie_breaker *)
-definition bar where "bar N t b G = (the_inv_into (G::goods) ((snd (t ((winning_allocations (b::bids) (G::goods) (N::participant set))::allocation set)))::participant \<Rightarrow> goods)::goods \<Rightarrow> participant)"
-*)
-
-definition payments (* :: "bids \<Rightarrow> goods \<Rightarrow> participant set \<Rightarrow> tie_breaker \<Rightarrow> participant \<Rightarrow> price" *)
-where "payments b G N t n = 
+definition payments_rel :: "bids \<Rightarrow> goods \<Rightarrow> participant set \<Rightarrow> tie_breaker_rel \<Rightarrow> participant \<Rightarrow> price"
+where "payments_rel b G N t n = 
   (let winning_allocation =
-    as_function (inverse (snd (t (winning_allocations b G N))))
+    as_function (inverse (snd (t (winning_allocations_rel b G N))))
    in
-     alpha b G N n - 
+     \<alpha> b G N n - 
      (\<Sum> m \<in> N - {n} . b m (winning_allocation m)) )"
 
+(*
 declare [[show_types]]
-
-
-
-notepad
-begin
-  def foo \<equiv> "(F bb) ` (possible_allocations {x::nat . True} {x::nat . True})"
-  def inv \<equiv> "the_inv_into {1::nat} (\<lambda>n::nat . n + 1)"
-end
-
+*)
 
 end
 
