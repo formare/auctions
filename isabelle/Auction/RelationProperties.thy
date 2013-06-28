bcdf177ed8e103e509f8cd948f7e64a34abf61c6
Christoph Lange <math.semantic.web@gmail.com>
2013-06-28 18:48:48 +0000

    enumerating all injective functions

--- isabelle/Auction/RelationProperties.thy
+++ isabelle/Auction/RelationProperties.thy
@@ -27,6 +27,11 @@ where "right_unique R \<longleftrightarrow> (\<forall> a \<in> Domain R . card (
 definition function_on :: "('a \<times> 'b) set \<Rightarrow> 'a set \<Rightarrow> bool"
 where "function_on R A \<longleftrightarrow> left_total_on R A \<and> right_unique R"
 
+definition as_part_fun :: "('a \<times> 'b) set \<Rightarrow> 'a \<rightharpoonup> 'b"
+where "as_part_fun R a = (let im = R `` {a} in 
+        if im = {} then None
+        else Some (THE b . b \<in> im))"
+
 definition as_function :: "('a \<times> 'b) set \<Rightarrow> 'a \<Rightarrow> 'b"
 where "as_function R a = (THE b . b \<in> R `` {a})"
 
@@ -43,5 +48,31 @@ where "injective R \<longleftrightarrow> (\<forall> a \<in> Domain R . \<forall>
 
 definition inverse :: "('a \<times> 'b) set \<Rightarrow> ('b \<times> 'a) set"
 where "inverse R = { (y, x) . (x, y) \<in> R }"
+
+fun \<beta> :: "'a \<Rightarrow> 'b set \<Rightarrow> ('a \<times> 'b) set \<Rightarrow> ('a \<times> 'b) set set"
+where "\<beta> x Y z = (\<lambda> foo . z \<union> {(x, foo)}) ` (Y - (Range z))"
+
+value "\<beta> True {1::nat,2,3} ` {{}, {(False, 1::nat)}}"
+
+(*
+fun \<alpha> :: "'a \<Rightarrow> 'b set \<Rightarrow> ('a \<times> 'b) set \<Rightarrow> (('a \<times> 'b) set \<times> 'a \<times> 'b) set" 
+where "\<alpha> x Y f = {f} \<times> ({x} \<times> (Y - (Range f)))"
+
+fun \<beta> where "\<beta> tup = fst tup \<union> { snd tup }"
+
+value "\<beta> (tup::(('a \<times> 'b) set \<times> 'a \<times> 'b) set)"
+
+fun \<gamma> where "\<gamma> x Y f = \<beta> (\<alpha> x Y f)"
+*)
+
+value "\<Union> {{1::nat,2}, {3,4::nat}}"
+
+(* TODO CL: Now that we can "easily" generate all total functions, maybe let's revert the "option type" stuff in nVCG.thy (which we introduced to allow for non-totality) *)
+fun injective_functions_list :: "'a list \<Rightarrow> 'b list \<Rightarrow> ('a \<times> 'b) set set"
+where "injective_functions_list [] ys = {{}}"
+    | "injective_functions_list (x # xs) ys = 
+       \<Union> ((\<beta> x (set ys)) ` (injective_functions_list xs ys))"
+
+value "injective_functions_list [False,True] [0::nat, 1, 2]"
 
 end
