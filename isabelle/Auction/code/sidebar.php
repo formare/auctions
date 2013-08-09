28224ace7d49debd9e4e078a294d2c03605b038e
Christoph Lange <math.semantic.web@gmail.com>
2013-08-09 20:54:11 +0000

    copying generated code to homepage

---
+++ isabelle/Auction/code/sidebar.php
@@ -0,0 +1,4 @@
+<li><a href="../..">Auction Theory Toolbox (ATT)</a></li>
+<li><a href="/research/projects/formare/">ForMaRE Project</a></li>
+<li><a href="http://isabelle.in.tum.de">Isabelle</a></li>
+<li><a href="http://scala-lang.org">Scala</a></li>
--- isabelle/index.php
+++ isabelle/index.php
@@ -15,9 +15,10 @@ PageStart();
 ?>
 <h2>Files relevant to the Isabelle formalisation</h2>
 <ul>
+  <li><a href="https://codex.cs.bham.ac.uk/svn/langec/formare/code/auction/isabelle">Source repository (Subversion)</a>.  You can browse the repository as user <code>guest</code> with password <code>guest</code>.</li>
   <li><a href="isabelle.zip">all-in-one ZIP archive for download</a></li>
   <li>*.thy: Isabelle formalisation:<ul>
-      <li>General foundations of single good auctions, and Vickrey's theorem:
+      <li>General foundations of static single good auctions, and Vickrey's theorem:
         <ul>
           <li><a href="Auction/All.thy">All.thy</a>: loads all theories whose formalisation is stable, for convenience</li>
           <li><a href="Auction/Vectors.thy">Vectors.thy</a>: vectors, implemented as functions</li>
@@ -29,27 +30,28 @@ PageStart();
           <li><a href="Auction/SecondPriceAuction.thy">SecondPriceAuction.thy</a>: second price single good auctions (underspecified; without tie-breaking) and some of their properties</li>
           <li><a href="Auction/Vickrey.thy">Vickrey.thy</a>: Vickrey's Theorem: second price auctions are efficient, and truthful bidding is a weakly dominant strategy.</li></ul>
       </li>
-      <li>Well-definedness checks for single good auctions:
+      <li>Soundness checks for static single good auctions:
         <ul>
-          <li><a href="Auction/SecondPriceAuctionCaseCheck.thy">SecondPriceAuction.thy</a>: well-definedness proof of the underspecified second price single good auctions without tie-breaking</li>
+          <li><a href="Auction/SecondPriceAuctionCaseCheck.thy">SecondPriceAuction.thy</a>: soundness proof of the underspecified second price single good auctions without tie-breaking</li>
           <li><a href="Auction/UniqueMaximum.thy">UniqueMaximum.thy</a>: determining a unique maximum components of vectors, subject to tie-breaking</li>
           <li><a href="Auction/SingleGoodAuctionTieBreaker.thy">SingleGoodAuctionTieBreaker.thy</a>: tie-breaking rules for single good auctions</li>
           <li><a href="Auction/FullySpecifiedSingleGoodAuction.thy">FullySpecifiedSingleGoodAuction.thy</a>: fully specified single good auctions, with tie-breaking rules</li>
           <li><a href="Auction/FullySpecifiedSecondPriceAuction.thy">FullySpecifiedSecondPriceAuction.thy</a>: fully specified variant of the second price single good auction</li>
-          <li><a href="Auction/FullySpecifiedSecondPriceAuctionCaseCheck.thy">FullySpecifiedSecondPriceAuctionCaseCheck.thy</a>: well-definednes proof of the fully specified variant of the second price single good auction</li>
+          <li><a href="Auction/FullySpecifiedSecondPriceAuctionCaseCheck.thy">FullySpecifiedSecondPriceAuctionCaseCheck.thy</a>: soundness proof of the fully specified variant of the second price single good auction</li>
       </ul></li>
-      <li>Combinatorial auctions, from foundations to well-definedness checks:
+      <li>Static combinatorial auctions, from foundations to soundness checks:
         <ul>
           <li><a href="Auction/RelationProperties.thy">RelationProperties.thy</a>: further helpful properties of relations, beyond Relations.thy from the library</li>
           <li><a href="Auction/Partitions.thy">Partitions.thy</a>: partitions of sets</li>
-          <li><a href="Auction/nVCG_CaseChecker.thy">nVCG_CaseChecker.thy</a>: well-definedness proof (in progress) of the fully specified combinatorial Vickrey auction</li>
+          <li><a href="Auction/nVCG_CaseChecker.thy">nVCG_CaseChecker.thy</a>: soundness proof (in progress) of the fully specified combinatorial Vickrey auction</li>
+          <li><a href="Auction/CombinatorialVickreyAuction.thy">CombinatorialVickreyAuction.thy</a>: wrapper to generate verified code to execute the combinatorial Vickrey auction</li>
         </ul>
       </li>
   </li>
   <li>*.pdf: exported files (for an old version of Vickrey's theorem at the time of this writing):<ul>
       <li><a href="Auction/output/document/root.pdf">human-readable LaTeX paper</a></li>
       <li><a href="graph.pdf">theory graph</a></li></ul></li>
-  <li>scala/: executable Scala code generated from the fully specified auctions (in progress)</li>
+  <li><a href="Auction/code/">code</a>: executable Scala code generated from the fully specified auctions (in progress)</li>
   <li><a href="Makefile">Makefile</a> contains rules to regenerate some of the files mentioned above.</li>
 </ul>
 <h2>How to run</h2>
