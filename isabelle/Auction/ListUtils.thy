8494a0cef29df9e06ad910de95a326f50c936013
Christoph Lange <math.semantic.web@gmail.com>
2013-08-29 12:34:14 +0200

    lemma ListUtils.distinct_map, and several lemmas about list-oriented partitions: * lemma remove_from_eq_class_preserves_disjoint * lemma insert_into_member_list_distinct * lemma partition_without_covers_list * reordered some lemmas by dependency to improve reusability

--- isabelle/Auction/ListUtils.thy
+++ isabelle/Auction/ListUtils.thy
@@ -40,4 +40,25 @@ lemma finite_imp_length_sort_eq_card: fixes x assumes "finite x"
 
 lemma remove_list_to_set: "set (x # removeAll y l) = insert x (set l - {y})" by simp
 
+text {* mapping a function @{text f} over a distinct list @{text xs} yields a distinct list,
+  if @{text f} returns unique results for each element of @{text xs}. *}
+lemma distinct_map:
+  shows "distinct xs \<Longrightarrow> \<forall> y \<in> set xs . f y \<notin> set (map f (remove1 y xs)) \<Longrightarrow> distinct (map f xs)"
+proof (induct xs)
+  case Nil
+  then show ?case by simp
+next
+  case (Cons x xs)
+  note distinct = Cons.prems(1)
+  note map_unique = Cons.prems(2)
+  have prev_goal: "distinct (map f xs)"
+    by (metis Cons.hyps distinct distinct.simps(2) in_set_member map.simps(2) map_unique member_rec(1) remove1.simps(2))
+  have "remove1 x xs = xs"
+    using distinct by (metis distinct.simps(2) remove1_idem)
+  then have "f x \<notin> set (map f xs)"
+    using map_unique by (metis not_Cons_self2 remove1.simps(2) remove1_idem)
+  then have "distinct (f x # map f xs)" by (metis distinct.simps(2) prev_goal)
+  then show ?case by simp
+qed
+
 end
