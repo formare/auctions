5afdb2e86ca7c62c7159a7d372e2c73829829d46
Christoph Lange <math.semantic.web@gmail.com>
2013-09-03 12:03:46 +0200

    using Manfred's gmail address except for TeX papers

--- isabelle/Auction/FullySpecifiedSingleGoodAuction.thy
+++ isabelle/Auction/FullySpecifiedSingleGoodAuction.thy
@@ -2,7 +2,7 @@
 Auction Theory Toolbox (http://formare.github.io/auctions/)
 
 Authors:
-* Manfred Kerber <m.kerber@cs.bham.ac.uk>
+* Manfred Kerber <mnfrd.krbr@gmail.com>
 * Christoph Lange <math.semantic.web@gmail.com>
 * Colin Rowat <c.rowat@bham.ac.uk>
 * Makarius Wenzel <wenzel@lri.fr>
