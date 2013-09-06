ad74260064e57ef83e4967c89d14ad6309265b7a
Christoph Lange <math.semantic.web@gmail.com>
2013-09-06 12:30:21 +0200

    new example where partial allocation would make sense

--- isabelle/Auction/CombinatorialVickreyAuctionTest.thy
+++ isabelle/Auction/CombinatorialVickreyAuctionTest.thy
@@ -55,5 +55,23 @@ value "hd (winning_allocations_comp_CL
 )"
 value "{(x, payments_comp sga_goods {0::nat, 1, 2} hd (sga_bids (nth [23::nat, 42, 31])) x) | x . x \<in> {0::nat, 1, 2}}"
 
+section {* a case where it is optimal not to allocate a good *}
+
+definition partial_alloc_participants where "partial_alloc_participants = {1::nat, 2}"
+definition partial_alloc_goods where "partial_alloc_goods = {(* A *) 98, (* B *) 99}"
+definition partial_alloc_bids where "partial_alloc_bids bidder goods = (
+      if (bidder = 1 \<and> goods = {98}) then 200
+      else if (bidder = 2 \<and> goods = {98,99}) then 100
+      else 0)"
+
+value "winning_allocations_comp_CL
+  partial_alloc_goods
+  partial_alloc_participants
+  partial_alloc_bids"
+
+value "possible_allocations_comp
+  partial_alloc_goods
+  partial_alloc_participants"
+
 end
 
