4abc5ca5be9288c75b776eb2c25042f34cd675b9
Christoph Lange <math.semantic.web@gmail.com>
2013-09-11 22:28:22 +0200

    easier proof of ListUtils.set_concat

--- isabelle/Auction/ListUtils.thy
+++ isabelle/Auction/ListUtils.thy
@@ -40,12 +40,7 @@ lemma finite_imp_length_sort_eq_card: fixes x assumes "finite x"
 
 lemma remove_list_to_set: "set (x # removeAll y l) = insert x (set l - {y})" by simp
 
-lemma set_concat: "set (concat xss) = \<Union> {set xs | xs . xs \<in> set xss}" (is "?LHS = ?RHS")
-proof
-  show "?LHS \<subseteq> ?RHS" by auto
-next
-  show "?RHS \<subseteq> ?LHS" by auto
-qed
+lemma set_concat: "set (concat xss) = \<Union> {set xs | xs . xs \<in> set xss}" by auto
 
 lemma list_comp_eq_set_comp:
   shows "set [ f x . x \<leftarrow> xs ] = { f x | x . x \<in> set xs }"
