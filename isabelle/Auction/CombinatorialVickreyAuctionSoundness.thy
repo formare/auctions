9d9a5466f2d196abdff3d7dbf6909a2939742c85
Christoph Lange <math.semantic.web@gmail.com>
2013-09-12 16:39:33 +0200

    CombinatorialVickreyAuctionSoundness.right_unique done

--- isabelle/Auction/CombinatorialVickreyAuctionSoundness.thy
+++ isabelle/Auction/CombinatorialVickreyAuctionSoundness.thy
@@ -40,6 +40,8 @@ proof (rule left_totalI)
     unfolding nVCG_auctions_def using pred_imp_rel_all by metis
 qed
 
+text {* The combinatorial Vickrey auction in relational form is right-unique.  This is easy to 
+  show because its outcome is defined by two functions, which are right-unique by construction. *}
 lemma right_unique:
   fixes t::tie_breaker_rel
   shows "right_unique (nVCG_auctions t) admissible_input"
@@ -48,11 +50,24 @@ proof (rule right_uniqueI)
   assume admissible: "admissible_input G N b"
   fix x::allocation_rel and x'::allocation_rel and p::payments and p'::payments
 
+  have *: "\<And> G' N' b' x'' p'' . ((G', N', b'), (x'', p'')) \<in> (nVCG_auctions t) \<Longrightarrow>
+    x'' = winning_allocation_rel G' N' t b' \<and> p'' = payments_rel G' N' t b'"
+  proof -
+    fix G' N' b' x'' p''
+    assume "((G', N', b'), (x'', p'')) \<in> (nVCG_auctions t)"
+    then have "pred_tup (nVCG_pred t) ((G', N', b'), (x'', p''))"
+      unfolding nVCG_auctions_def rel_all_def by fast
+    then show "x'' = winning_allocation_rel G' N' t b' \<and> p'' = payments_rel G' N' t b'"
+      unfolding pred_tup_def nVCG_pred_def by blast
+  qed
+
   assume "((G, N, b), (x, p)) \<in> (nVCG_auctions t)"
-
+  then have xp: "x = winning_allocation_rel G N t b \<and> p = payments_rel G N t b" by (rule *)
+    
   assume "((G, N, b), (x', p')) \<in> (nVCG_auctions t)"
+  then have xp': "x' = winning_allocation_rel G N t b \<and> p' = payments_rel G N t b" by (rule *)
 
-  show "x = x' \<and> p = p'" sorry
+  from xp xp' show "x = x' \<and> p = p'" by fast
 qed
 
 end
