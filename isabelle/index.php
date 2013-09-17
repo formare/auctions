3787a0f750fd3d7be69231c9cd2eb62b0af639ab
Christoph Lange <math.semantic.web@gmail.com>
2013-09-17 11:52:20 +0200

    Isabelle formalisation homepage up to date

--- isabelle/index.php
+++ isabelle/index.php
@@ -18,11 +18,24 @@ PageStart();
   <li><a href="https://github.com/formare/auctions/tree/master/isabelle">Source repository on GitHub</a>.</li>
   <li><a href="isabelle.zip">all-in-one ZIP archive for download</a></li>
   <li>*.thy: Isabelle formalisation:<ul>
+      <li><a href="Auction/All.thy">All.thy</a>: loads all theories whose formalisation is stable, for convenience</li>
+      <li>General foundations; additions to the Isabelle library:
+        <ul>
+          <li><a href="Auction/Finite_SetUtils.thy">Finite_SetUtils.thy</a>: additions to Finite_Set.thy from the library</li>
+          <li><a href="Auction/HOLUtils.thy">HOLUtils.thy</a>: additions to HOL.thy from the library</li>
+          <li><a href="Auction/ListUtils.thy">ListUtils.thy</a>: additions to List.thy from the library</li>
+          <li><a href="Auction/Maximum.thy">Maximum.thy</a>: maximum components of vectors and sets, and their properties</li>
+          <li><a href="Auction/Partitions.thy">Partitions.thy</a>: partitions of sets</li>
+          <li><a href="Auction/PartitionsAlternative.thy">PartitionsAlternative.thy</a>: some alternative definitions of partitions (currently unused)</li>
+          <li><a href="Auction/PartitionsTest.thy">PartitionsTest.thy</a>: test cases for partitions</li>
+          <li><a href="Auction/RelationOperators.thy">RelationOperators.thy</a>: some additional operators on relations, and their properties</li>
+          <li><a href="Auction/RelationProperties.thy">RelationProperties.thy</a>: further properties of the library operators on relations</li>
+          <li><a href="Auction/RelationUtils.thy">RelationUtils.thy</a>: additions to Relations.thy from the library</li>
+          <li><a href="Auction/SetUtils.thy">SetUtils.thy</a>: additions to Set.thy from the library</li>
+          <li><a href="Auction/Vectors.thy">Vectors.thy</a>: vectors, implemented as functions</li>
+      </ul></li>
       <li>General foundations of static single good auctions, and Vickrey's theorem:
         <ul>
-          <li><a href="Auction/All.thy">All.thy</a>: loads all theories whose formalisation is stable, for convenience</li>
-          <li><a href="Auction/Vectors.thy">Vectors.thy</a>: vectors, implemented as functions</li>
-          <li><a href="Auction/Maximum.thy">Maximum.thy</a>: maximum components of vectors, and their properties</li>
           <li><a href="Auction/MaximumTest.thy">MaximumTest.thy</a>: some examples for testing Maximum</li>
           <li><a href="Auction/SingleGoodAuction.thy">SingleGoodAuction.thy</a>: single good auctions</li>
           <li><a href="Auction/SingleGoodAuctionTest.thy">SingleGoodAuctionTest.thy</a>: some examples for testing SingleGoodAuction</li>
@@ -32,19 +45,24 @@ PageStart();
       </li>
       <li>Soundness checks for static single good auctions:
         <ul>
-          <li><a href="Auction/SecondPriceAuctionCaseCheck.thy">SecondPriceAuction.thy</a>: soundness proof of the underspecified second price single good auctions without tie-breaking</li>
+          <li><a href="Auction/SecondPriceAuctionSoundness.thy">SecondPriceAuctionSoundness.thy</a>: soundness proof of the underspecified second price single good auctions without tie-breaking</li>
           <li><a href="Auction/UniqueMaximum.thy">UniqueMaximum.thy</a>: determining a unique maximum components of vectors, subject to tie-breaking</li>
           <li><a href="Auction/SingleGoodAuctionTieBreaker.thy">SingleGoodAuctionTieBreaker.thy</a>: tie-breaking rules for single good auctions</li>
           <li><a href="Auction/FullySpecifiedSingleGoodAuction.thy">FullySpecifiedSingleGoodAuction.thy</a>: fully specified single good auctions, with tie-breaking rules</li>
           <li><a href="Auction/FullySpecifiedSecondPriceAuction.thy">FullySpecifiedSecondPriceAuction.thy</a>: fully specified variant of the second price single good auction</li>
-          <li><a href="Auction/FullySpecifiedSecondPriceAuctionCaseCheck.thy">FullySpecifiedSecondPriceAuctionCaseCheck.thy</a>: soundness proof of the fully specified variant of the second price single good auction</li>
+          <li><a href="Auction/FullySpecifiedSecondPriceAuctionSoundness.thy">FullySpecifiedSecondPriceAuctionSoundness.thy</a>: soundness proof of the fully specified variant of the second price single good auction</li>
+          <li><a href="Auction/FullySpecifiedSecondPriceAuctionCode.thy">FullySpecifiedSecondPriceAuctionCode.thy</a>: wrapper to generate verified code to execute the fully specified second price single good auction</li>
       </ul></li>
       <li>Static combinatorial auctions, from foundations to soundness checks:
         <ul>
-          <li><a href="Auction/RelationProperties.thy">RelationProperties.thy</a>: further helpful properties of relations, beyond Relations.thy from the library</li>
-          <li><a href="Auction/Partitions.thy">Partitions.thy</a>: partitions of sets</li>
-          <li><a href="Auction/nVCG_CaseChecker.thy">nVCG_CaseChecker.thy</a>: soundness proof (in progress) of the fully specified combinatorial Vickrey auction</li>
-          <li><a href="Auction/CombinatorialVickreyAuction.thy">CombinatorialVickreyAuction.thy</a>: wrapper to generate verified code to execute the combinatorial Vickrey auction</li>
+          <li><a href="Auction/CombinatorialAuction.thy">CombinatorialAuction.thy</a>: definition of general combinatorial auctions</li>
+          <li><a href="Auction/CombinatorialAuctionProperties.thy">CombinatorialAuctionProperties.thy</a>: some properties that general combinatorial auctions can have</li>
+          <li><a href="Auction/CombinatorialAuctionTest.thy">CombinatorialAuctionTest.thy</a>: test combinatorial auctions</li>
+          <li><a href="Auction/CombinatorialAuctionTieBreaker.thy">CombinatorialAuctionTieBreaker.thy</a>: tie-breakers for combinatorial auctions</li>
+          <li><a href="Auction/CombinatorialVickreyAuction.thy">CombinatorialVickreyAuction.thy</a>: definition of the combinatorial Vickrey auction</li>
+          <li><a href="Auction/CombinatorialVickreyAuctionCode.thy">CombinatorialVickreyAuction.thy</a>: wrapper to generate verified code to execute the combinatorial Vickrey auction</li>
+          <li><a href="Auction/CombinatorialVickreyAuctionSoundness.thy">CombinatorialVickreyAuctionSoundness.thy</a>: soundness proof (in progress) of the fully specified combinatorial Vickrey auction</li>
+          <li><a href="Auction/CombinatorialVickreyAuctionTest.thy">CombinatorialVickreyAuctionTest.thy</a>: tests for the combinatorial Vickrey auction</li>
         </ul>
       </li>
   </li>
