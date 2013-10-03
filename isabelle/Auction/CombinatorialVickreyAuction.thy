formare/auctions

commit ad75bfb42f47c6fc9fd731be6c7d5887a2624137

got paper-style arg max right, a bit more progress in wd_outcome

isabelle/Auction/CombinatorialVickreyAuction.thy
@@ -23,25 +23,12 @@ imports
 
 begin
 
-section {* maximum value *}
-
-text {* the maximum value (according to the bids submitted) over all possible allocations (assuming
-  relational allocations) *}
-fun max_value :: "goods \<Rightarrow> participant set \<Rightarrow> bids \<Rightarrow> price"
-where "max_value G N b = Max ((value_rel b) ` (possible_allocations_rel G N))"
-(* CL: we don't need the variant that assumes functional allocations, as it's really just the same *)
-
-text {* algorithmic version of @{const max_value} *}
-fun max_value_alg :: "goods \<Rightarrow> participant set \<Rightarrow> bids \<Rightarrow> price"
-where "max_value_alg G N b = maximum_alg_list (possible_allocations_alg G N) (value_rel b)"
-
 section {* value-maximising allocation (for winner determination) *}
 
-text {* the set of value-maximising allocations, i.e.\ the ``arg max'' where 
-  @{const max_value} is the ``max'' *}
+text {* the set of value-maximising allocations (according to the bids submitted), i.e.\ the ``arg max''
+  of @{const value_rel} on the set of all possible allocations *}
 definition winning_allocations_rel :: "goods \<Rightarrow> participant set \<Rightarrow> bids \<Rightarrow> allocation_rel set"
-where "winning_allocations_rel G N b = 
-{ potential_buyer . value_rel b potential_buyer = max_value G N b }"
+where "winning_allocations_rel G N b = arg_max' (value_rel b) (possible_allocations_rel G N)"
 
 (* CL: probably not needed, neither for close-to-paper nor for computable version
 definition winning_allocations_fun :: "goods \<Rightarrow> participant set \<Rightarrow> bids \<Rightarrow> allocation_fun set"
@@ -83,11 +70,11 @@ text {* payments *}
 text {* the maximum sum of bids of all bidders except bidder @{text n}'s bid, computed over all possible allocations of all goods,
   i.e. the value reportedly generated by value maximization problem when solved without n's bids *}
 fun \<alpha> :: "goods \<Rightarrow> participant set \<Rightarrow> bids \<Rightarrow> participant \<Rightarrow> price"
-where "\<alpha> G N b n = max_value G (N - {n}) b"
+where "\<alpha> G N b n = Max ((value_rel b) ` (possible_allocations_rel G (N - {n})))"
 
 text {* algorithmic version of @{text \<alpha>} *}
 fun \<alpha>_alg :: "goods \<Rightarrow> participant set \<Rightarrow> bids \<Rightarrow> participant \<Rightarrow> price"
-where "\<alpha>_alg G N b n = max_value_alg G (N - {n}) b"
+where "\<alpha>_alg G N b n = maximum_alg_list (possible_allocations_alg G N) (value_rel b)"
 
 (* CL: probably not needed, neither for close-to-paper nor for computable version
 definition winners'_goods_fun :: "goods \<Rightarrow> participant set \<Rightarrow> tie_breaker_fun \<Rightarrow> bids \<Rightarrow> participant option \<Rightarrow> goods" 

isabelle/Auction/CombinatorialVickreyAuctionSoundness.thy
@@ -99,29 +99,59 @@ proof (rule wd_outcomeI)
   then have xp: "x = winning_allocation_rel G N t b \<and> p = payments_rel G N t b" by (rule split_outcome)
 
   assume admissible: "admissible_input G N b"
-  from xp have "x = t { potential_buyer . value_rel b potential_buyer
-     = Max ((value_rel b) ` \<Union> { injections Y N | Y . Y \<in> all_partitions G }) }"
+  from xp have "x = t (arg_max' (value_rel b) (possible_allocations_rel G N))"
     unfolding winning_allocation_rel.simps winning_allocations_rel_def
     by simp
-  with assms have x_unfolded': "value_rel b x
-     = Max ((value_rel b) ` \<Union> { injections Y N | Y . Y \<in> all_partitions G })"
-     using tie_breaker_def by blast
+  then have x_unfolded: "x = t { x \<in> \<Union> { injections Y N | Y . Y \<in> all_partitions G } .
+    value_rel b x = Max ((value_rel b) ` (\<Union> { injections Y N | Y . Y \<in> all_partitions G })) }"
+    by simp
+  
   from xp (* to use Max_in, we need additional assumptions about N and G, so that \<Union> is non-empty *)
     have p_unfolded: "p = (\<lambda>n . (Max ((value_rel b) ` (possible_allocations_rel G (N - {n}))))
       - (\<Sum> m \<in> N - {n} . b m (eval_rel_or (x\<inverse>) m {})))" by fastforce
   
   have "wd_allocation G N x" unfolding wd_allocation_def
   proof
+    (* TODO CL: Fix the "sorry" once we know how to consistently define tie_breaker *)
+    from x_unfolded have x_alloc: "x \<in> \<Union> { injections Y N | Y . Y \<in> all_partitions G }" sorry
+
     show "is_partition_of (Domain x) G"
     proof -
-      from admissible have "G \<noteq> {}" and "N \<noteq> {}" unfolding admissible_input_def by simp_all
-      from `G \<noteq> {}` have "all_partitions G \<noteq> {}" by (rule non_empty_set_has_partitions)
-      then obtain Y where "\<Union> Y = G" unfolding all_partitions_def is_partition_of_def by blast
-      with `G \<noteq> {}` have "Y \<noteq> {}" by fast
-      
-        
-      show ?thesis sorry
+      from x_alloc have "\<exists> Y \<in> all_partitions G . x \<in> injections Y N" by (rule Union_map_member)
+      then obtain Y where part: "Y \<in> all_partitions G" and inj: "x \<in> injections Y N" by fast
+      from part have "is_partition_of Y G" unfolding all_partitions_def by (rule CollectD)
+      moreover have "Domain x = Y" using inj unfolding injections_def by simp
+      ultimately show ?thesis by blast
     qed
+
+    { (* CL: not yet sure what we need this for *)
+      from admissible have "card G > 0" and "card N > 0" unfolding admissible_input_def by simp_all
+
+      from `card G > 0` have "G \<noteq> {}" by force
+      then have "is_partition_of {G} G" by (rule set_partitions_itself)
+      then have *: "{G} \<in> all_partitions G" unfolding all_partitions_def by (rule CollectI)
+      moreover have "injections {G} N \<noteq> {}"
+      proof -
+        have "finite {G}" by simp
+        moreover have "finite N" using `card N > 0` by (rule card_ge_0_finite)
+        moreover have "card {G} \<le> card N" using `card N > 0` by auto
+        ultimately show ?thesis by (rule injections_exist)
+      qed
+      ultimately have non_empty: "\<Union> { injections Y N | Y . Y \<in> all_partitions G } \<noteq> {}" 
+        by (auto simp add: Union_map_non_empty)
+      have finite: "finite (\<Union> { injections Y N | Y . Y \<in> all_partitions G })" sorry
+        (* use finite_UN_I *)
+
+      note x_alloc
+      moreover have "finite ((value_rel b) ` \<Union> { injections Y N | Y . Y \<in> all_partitions G })"
+        using finite by simp
+      moreover have "(value_rel b) ` \<Union> { injections Y N | Y . Y \<in> all_partitions G } \<noteq> {}"
+        using non_empty by simp
+      ultimately have "value_rel b x \<in> (value_rel b) ` \<Union> { injections Y N | Y . Y \<in> all_partitions G }"
+        by simp (* Max_in *)
+
+    }
+
     show "Range x \<subseteq> N" sorry
   qed
   moreover have "wd_payments N p" unfolding wd_payments_def

commit e1b438378533eb8c7c8264efeea11d04d2815ce3

fixed a bug that was introduced in commit ad75bfb42f47c6fc9fd731be6c7d5887a2624137

isabelle/Auction/CombinatorialVickreyAuction.thy
@@ -75,7 +75,7 @@ where "\<alpha> G N b n = Max ((value_rel b) ` (possible_allocations_rel G (N - 
 
 text {* algorithmic version of @{text \<alpha>} *}
 fun \<alpha>_alg :: "goods \<Rightarrow> participant set \<Rightarrow> bids \<Rightarrow> participant \<Rightarrow> price"
-where "\<alpha>_alg G N b n = maximum_alg_list (possible_allocations_alg G N) (value_rel b)"
+where "\<alpha>_alg G N b n = maximum_alg_list (possible_allocations_alg G (N - {n})) (value_rel b)"
 
 (* CL: probably not needed, neither for close-to-paper nor for computable version
 definition winners'_goods_fun :: "goods \<Rightarrow> participant set \<Rightarrow> tie_breaker_fun \<Rightarrow> bids \<Rightarrow> participant option \<Rightarrow> goods" 
@@ -121,21 +121,6 @@ fun payments_alg :: "goods \<Rightarrow> participant set \<Rightarrow> tie_break
 where "payments_alg G N t = \<alpha>_alg G N - remaining_value_alg G N t"
 
 text {* alternative algorithmic version of @{text payments_rel}, working around an Isabelle2013 bug *}
-(* CL: expanded definition to work around https://lists.cam.ac.uk/pipermail/cl-isabelle-users/2013-July/msg00011.html
-   until Isabelle2014 fixes the bug; see https://lists.cam.ac.uk/pipermail/cl-isabelle-users/2013-July/msg00024.html) *)
-fun payments_alg_workaround :: "goods \<Rightarrow> participant set \<Rightarrow> tie_breaker_alg \<Rightarrow> bids \<Rightarrow> participant \<Rightarrow> price"
-where "payments_alg_workaround G N t b n = 
-  (* \<alpha>_alg G N *) maximum_alg_list (possible_allocations_alg G N) (value_rel b)
-  -
-  (* remaining_value_alg G N t *) (\<Sum> m \<in> N - {n} . b m (eval_rel_or
-    (* When a participant doesn't gain any goods, there is no participant \<times> goods pair in this relation,
-       but we interpret this case as if 'the empty set' had been allocated to the participant. *)
-    (
-      (* the winning allocation after tie-breaking: a goods \<times> participant relation, which we have to invert *)
-      (winning_allocation_alg_CL G N t b)\<inverse>)
-    m (* evaluate the relation for participant m *)
-    {} (* return the empty set if nothing is in relation with m *)
-  ))"
 
 section {* the Combinatorial Vickrey Auction in relational form *}
 