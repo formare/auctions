03690d0444b7724b3703cc279d8908eec3b9bf43
Christoph Lange <math.semantic.web@gmail.com>
2014-01-24 18:28:31 +0100

    fixed spurious variable

--- isabelle/Auction/Sandbox/p.thy
+++ isabelle/Auction/Sandbox/p.thy
@@ -7,8 +7,9 @@ begin
 value "all_partitions_alg {1::nat}"
 term "all_partitions {1::nat}"
 
-lemma assumes "x \<in> all_partitions X" shows 
-"EX y. y \<in> all_partitions (X \<union> Y) \<and> x \<subseteq> y'"
+lemma
+  assumes "x \<in> all_partitions X"
+  shows "\<exists> y. y \<in> all_partitions (X \<union> Y) \<and> x \<subseteq> y"
 proof -
 show ?thesis sorry
 qed
