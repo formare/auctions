8c08ae34a518335e7d291b62412198b51775baa0
Christoph Lange <math.semantic.web@gmail.com>
2014-01-27 13:43:03 +0100

    further progress on sum

--- isabelle/Auction/Sandbox/p.thy
+++ isabelle/Auction/Sandbox/p.thy
@@ -6,17 +6,26 @@ begin
 
 lemma
   assumes new: "B \<inter> A = {}"
-      and non_emptyA: "A \<noteq> {}"
-      and non_emptyB: "B \<noteq> {}"
+      and non_empty: "B \<noteq> {}"
     (* TODO CL: assume something about f *)
   shows "\<forall> P . P partitions A \<longrightarrow>
     (\<exists> Q . Q partitions (A \<union> B)
       \<and> (\<Sum> X \<in> P . f X) \<le> (\<Sum> Y \<in> Q . f Y))"
-proof
-  fix P assume part: "P partitions A"
-oops
+proof (rule allImpI)
+  fix P assume partA: "P partitions A"
 
-  (*
-  shows "\<exists> Q . Q partitions (A \<union> B) \<and> (\<forall> X \<in> P . \<exists> Y \<in> Q . X \<subseteq> Y)"
-  *)
+  then have "(P \<union> {B}) partitions (A \<union> B) \<and> (\<forall> X \<in> P . \<exists> Y \<in> P \<union> {B} . X \<subseteq> Y)"
+    using new non_empty
+    by (rule exists_partition_of_strictly_larger_set)
+  then have partB: "(P \<union> {B}) partitions (A \<union> B)"
+        and sub: "\<forall> X \<in> P . \<exists> Y \<in> P \<union> {B} . X \<subseteq> Y" by simp_all
+
+  have "(\<Sum> X \<in> P . f X) \<le> (\<Sum> Y \<in> P \<union> {B} . f Y)"
+  proof -
+    show ?thesis sorry
+  qed
+  with partB show "\<exists> Q . Q partitions (A \<union> B)
+    \<and> (\<Sum> X \<in> P . f X) \<le> (\<Sum> Y \<in> Q . f Y)" by blast
+qed
+
 end
