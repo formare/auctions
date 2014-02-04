a79a32c0b213603c493ff29bae4835746d3f6b31
caminati <spam.caminati@yahoo.com>
2014-02-04 19:55:13 +0000

    price nonnegativity completed

--- isabelle/Auction/Sandbox/g.thy
+++ isabelle/Auction/Sandbox/g.thy
@@ -32,6 +32,21 @@ using assms ll71 by blast
 lemma lll78: assumes "runiq (P^-1)" shows "P``(Domain P -X) \<inter> (P``X) = {}"
 using assms ll71 by fast
 
+lemma lll83: "P``X = P``(X \<inter> Domain P)" by blast
+
+lemma lll84: "P``(X \<inter> Domain P)=P``X" by blast
+
+lemma lll85: "Range (P||X) = P``X" 
+proof -
+  let ?p="P||X" let ?d=Domain let ?r=Range
+  have "?r ?p=?p``(?d ?p)" by auto moreover have 
+  "... = ?p``(X \<inter> ?d ?p)" using restrict_def by blast moreover have 
+  "... \<subseteq> P``(X \<inter> ?d ?p)" using restrict_def by auto
+  moreover have "... = P``X" by (metis Image_within_domain inf_commute inf_left_idem ll41)
+  moreover have "P``X \<subseteq> ?r ?p" using restrict_def by fast
+  ultimately show ?thesis by simp
+qed
+
 abbreviation "eval_rel2 R x == \<Union> (R``{x})"
 notation eval_rel2 (infix ",,," 75)
 (* MC: realized that eval_rel2 could be preferable to eval_rel, because it generalizes the latter 
@@ -149,23 +164,37 @@ proof -
   let ?z1="(xx, yy1)" let ?z2="(xx, yy2)" let ?a0="a -- xx" let ?a1="a +< ?z1" let ?a2="a +< ?z2"
   have 
   0: "{?z1} || {xx}={?z1} & {?z2}||{xx}={?z2}" using restrict_def by auto
+
+  have "finite {?z1} & finite {?z2}" by simp then have 
+  2: "finite ?a1 & finite ?a2" using paste_def assms 
+  by (metis finite_Un finite_insert outside_union_restrict)
+
   have "?a1 = (?a1 -- xx) \<union> (?a1 || {xx}) " 
   using paste_def Outside_def outside_union_restrict by metis
-  moreover have "finite ?a1" sorry
-  ultimately have "setsum b ?a1 = setsum b (?a1||{xx}) + setsum b (?a1 outside {xx})" 
+  have "setsum b ?a1 = setsum b (?a1||{xx}) + setsum b (?a1 outside {xx})" using 2
   by (metis finite_Un lll00 lll01 lll06b outside_union_restrict setsum.union_disjoint)
   moreover have 
-  1: "?a1 = a +* {?z1} & ?a2 = a +* {?z2}"
-  by (metis fst_conv snd_conv)
-  then have "?a1||{xx} = (a || {xx}) +* ({?z1} || {xx})" using lll71
-  by fastforce
+  1: "?a1 = a +* {?z1} & ?a2 = a +* {?z2}" by (metis fst_conv snd_conv)
+  then have "?a1||{xx} = (a || {xx}) +* ({?z1} || {xx})" using lll71 by fastforce
+
   moreover have "... = {?z1}||{xx}" using ll41 ll56 by (metis "0" Domain_empty Domain_insert Int_lower2)
   ultimately have 
   "setsum b ?a1 = setsum b ({?z1}) + setsum b (?a1 outside {xx})" 
   by (metis "0") then have
   11: "setsum b ?a1 = b ?z1 + setsum b (?a1 outside {xx})"
   by simp
-  have "?a1 = (?a1 -- xx) \<union> (?a1 || {xx}) " 
+
+  have "setsum b ?a2 = setsum b (?a2||{xx}) + setsum b (?a2 outside {xx})" using 2
+  by (metis finite_Un lll00 lll01 lll06b outside_union_restrict setsum.union_disjoint)
+  have "?a2||{xx} = (a || {xx}) +* ({?z2} || {xx})" using lll71 by fastforce
+  moreover have "... = {?z2}||{xx}" using ll41 ll56 by (metis "0" Domain_empty Domain_insert Int_lower2)
+  ultimately have "setsum b ?a2 = setsum b ({?z2}) + setsum b (?a2 outside {xx})" using 1 0  by (metis 
+  `proceeds b (a +< (xx, yy2)) = proceeds b ((a +< (xx, yy2)) || {xx}) + proceeds b ((a +< (xx, yy2)) -- xx)`) 
+  then have
+  12: "setsum b ?a2 = b ?z2 + setsum b (?a2 outside {xx})" by simp
+
+(*
+  have "?a2 = (?a2 -- xx) \<union> (?a2 || {xx})" 
   using paste_def Outside_def outside_union_restrict by metis
   moreover
   have "finite ?a2" sorry
@@ -174,10 +203,12 @@ proof -
   moreover have "... = {?z2}||{xx}" using ll41 ll56 
   by (metis (hide_lams, no_types) "0" Domain_empty Domain_insert Int_lower2)
   ultimately have "setsum b ?a2 = setsum b ({?z2}) + setsum b (?a2 outside {xx})" 
-  using 0 sorry
+  using 0 2 sledgehammer
   then have
   12: "setsum b ?a2 = b ?z2 + setsum b (?a2 outside {xx})"
   by simp
+*)
+
   have "?a1 outside {xx} = (a outside {xx}) +* ({?z1} outside {xx})" 
   using lll72 by (metis "1")
   moreover have "... = (a outside {xx}) +* {}" using 1
@@ -190,44 +221,72 @@ proof -
   ultimately show ?thesis using 11 12 assms by smt
 qed
 
-lemma lll76: 
-assumes "a \<in> possible_allocations_rel G N"
+lemma lll86: assumes "X \<inter> Y={}" shows "R``X = (R outside Y)``X"
+using assms Outside_def Image_def by blast
+
+lemma lll76: assumes "a \<in> possible_allocations_rel G N"
+"n \<in> Range a"
+"finite (possible_allocations_rel G (N-{n}))" (*MC: qv allocs_finite *)
+"finite a" (*MC: the two finiteness requirements can be replaced by finiteness of N, G*)
+"EX i. i\<in>Domain (a^-1 -- n) & b (i, (a^-1),,,i) \<le> b (i, (a^-1),,,i \<union> (a^-1),,,n)"
+(* MC: this is monotonicity assumption *)
 shows "Max (proceeds b ` (converse ` (possible_allocations_rel G (N - {n})))) \<ge> 
 proceeds b ((a^-1) -- n)"
 proof -
   let ?P=possible_allocations_rel let ?aa="a^-1 -- n" let ?d=Domain let ?Yn="a^-1,,,n"
-  let ?p=proceeds let ?X="converse ` (?P G (N-{n}))" let ?u=runiq let ?r=Range
-  have 
-2: "?aa \<noteq> {}& ?u ?aa & ?u a & ?u (a^-1) & ?u (?aa^-1)" sorry then
-  obtain i where 
-  0: "i \<in> ?d ?aa" by auto
+  let ?p=proceeds let ?X="converse ` (?P G (N-{n}))" let ?u=runiq let ?r=Range 
+
+  have "?u a & ?u (a^-1)" using assms(1) lll81 by blast
+  then moreover have "?u ?aa" using subrel_runiq Outside_def by blast
+  moreover have "?aa \<subseteq> a^-1" using Outside_def by blast
+  then moreover have "?aa^-1 \<subseteq> a" using Outside_def converse_def by (metis converse_subset_swap)
+  ultimately have 
+  2: "?u ?aa & ?u a & ?u (a^-1) & ?u (?aa^-1)" using subrel_runiq by auto obtain i where 
+  0: "i \<in> ?d ?aa & b (i, (a^-1),,,i) \<le> b (i, (a^-1),,,i \<union> ?Yn)" using assms(5) by blast
   let ?Y1="?aa,,,i" let ?Y2="?Y1 \<union> ?Yn"
-have 
-5: "?d ?aa \<subseteq> N - {n}" using assms lll81 by (metis Diff_mono Range_converse converse_converse outside_reduces_domain subset_refl)
-then have 
-6: "N - {n} \<union> {i} = N -{n}" using 0 by blast
-have
-3: "is_partition_of (?d a) G" using assms lll81 by blast then
-have "is_partition (?r (a^-1))" using  is_partition_of_def by (metis Range_converse)
-then have 
-4: "is_partition (?r ?aa)" using all_partitions_def is_partition_of_def 
-Outside_def subset_is_partition lll81 assms by (metis Range_outside_sub equalityE)
-moreover have "?Y1 \<in> (?r ?aa)" using 0 lll82 by (metis "2" eval_runiq_in_Range)
-ultimately have "?Y2 \<noteq> {}" using is_partition_def 0 by (metis Un_empty  inf_bot_right)
+  
+  have "{i} \<inter> {n}={}" using 0 by (metis Diff_iff Int_commute Int_empty_right Int_insert_right_if0 outside_reduces_domain)
+  then have "?aa``{i} = (a^-1)``{i}" using 0 Outside_def Image_def lll86 by metis then
+  have 
+  7: "?Y1=(a^-1),,,i" by simp
+
+  have 
+  5: "?d ?aa \<subseteq> N - {n}" using assms lll81 by (metis Diff_mono Range_converse converse_converse outside_reduces_domain subset_refl)
+  then have 
+  6: "N - {n} \<union> {i} = N -{n}" using 0 by blast
+  have
+  3: "is_partition_of (?d a) G" using assms lll81 by blast then
+  have "is_partition (?r (a^-1))" using  is_partition_of_def by (metis Range_converse)
+  then have 
+  4: "is_partition (?r ?aa)" using all_partitions_def is_partition_of_def 
+  Outside_def subset_is_partition lll81 assms by (metis Range_outside_sub equalityE)
+  moreover have "?Y1 \<in> (?r ?aa)" using 0 lll82 by (metis "2" eval_runiq_in_Range)
+  ultimately have "?Y2 \<noteq> {}" using is_partition_def 0 by (metis Un_empty  inf_bot_right)
 
   have "{i} \<times> ?aa``{i} = {i} \<times> {?Y1}" using 0 Image_runiq_eq_eval 2 by (metis cSup_singleton)
   moreover have "... = {(i, ?Y1)}" by simp
   ultimately have 
   1: "?aa +< (i, ?Y1) = ?aa" using 0 paste_def eval_rel_def ll84 by (metis fst_conv snd_conv)
+  
+  have "?r (a^-1) = ?r ?aa \<union> ?r ((a^-1)||{n})" by (metis Range_Un_eq outside_union_restrict)
+  moreover have "... = ?r ?aa \<union> (a^-1) `` {n}" by (metis lll85)
+  ultimately moreover have "?r ?aa = (a^-1)``(?d (a^-1)-{n})" by (metis lll01 lll85)
+  ultimately moreover have "?r (a^-1) = ?r ?aa \<union> (a^-1)``{n}" by simp
+  ultimately moreover have "?r ?aa \<inter> (a^-1)``{n} = {}" using lll78
+  by (metis "2" converse_converse)
+  ultimately have "?r ?aa=?r (a^-1) - (a^-1)``{n}" by blast
+  moreover have "a^-1``{n} = {?Yn}"
+  by (metis "2" Domain_converse Image_runiq_eq_eval assms(2) lll82)
+  ultimately have "?r ?aa = ?r (a^-1) - {?Yn}" by force
+  moreover have "{?Yn} \<subseteq> ?r (a^-1)" using assms eval_runiq_in_Range by (metis "2" Domain_converse cSup_singleton empty_subsetI insert_subset runiq_wrt_eval_rel')
+  moreover have "\<Union> (?r (a^-1))=G" using assms lll81 is_partition_of_def by (metis Range_converse)
+  ultimately
+  have "is_partition_of (?r ?aa) (G - ?Yn)" using lll80 3 2 4 
+  by (metis `is_partition (Range (a\<inverse>))` cSup_singleton)
+  moreover have "?u ?aa" by (metis "2")
+  moreover have "?u (?aa^-1)" using 2 by fast
+  moreover have "?d ?aa \<subseteq> (N -{n})" by (metis Diff_mono Domain_converse assms lll81 outside_reduces_domain subset_refl)
 
-have "?r ?aa = ?r (a^-1) - {?Yn}" using 2 sorry
-moreover have "{?Yn} \<subseteq> ?r (a^-1)" sorry
-moreover have "\<Union> (?r (a^-1))=G" sorry
-ultimately
-have "is_partition_of (?r ?aa) (G - ?Yn)" using lll80 3 2 4 by (metis `is_partition (Range (a\<inverse>))` cSup_singleton)
-moreover have "?u ?aa" by (metis "2")
-moreover have "?u (?aa^-1)" using 2 by fast
-moreover have "?d ?aa \<subseteq> (N -{n})" by (metis Diff_mono Domain_converse assms lll81 outside_reduces_domain subset_refl)
   ultimately have "?aa^-1 \<in> ?P (G-?Yn) (N-{n})" using assms lll81 by (metis Domain_converse converse_converse)
   moreover have "?Y2 \<inter> (G -?Yn - ?Y1)={}" by fast
   ultimately have "(?aa +< (i, ?Y2))^-1 \<in> ?P (G - ?Yn - ?Y1 \<union> ?Y2) (N-{n} \<union> {i})" 
@@ -235,30 +294,33 @@ moreover have "?d ?aa \<subseteq> (N -{n})" by (metis Diff_mono Domain_converse 
   then have 
   "(?aa +< (i, ?Y2))^-1 \<in> ?P (G \<union> ?Y2) (N-{n} \<union> {i})" by (smt Un_Diff_cancel Un_commute Un_left_commute)
 
-moreover have "?Yn \<subseteq> G"
-by (metis Union_upper `\<Union>Range (a\<inverse>) = G` `{a\<inverse> ,,, n} \<subseteq> Range (a\<inverse>)` insert_subset)
-moreover have "\<Union> (?r ?aa) \<subseteq> G"
-by (metis Diff_subset Sup_subset_mono `Range (a\<inverse> -- n) = Range (a\<inverse>) - {a\<inverse> ,,, n}` `\<Union>Range (a\<inverse>) = G`)
-then moreover have "?Y1 \<subseteq> G" 
-by (metis Sup_le_iff `(a\<inverse> -- n) ,,, i \<in> Range (a\<inverse> -- n)`)
-ultimately moreover have "?Y2 \<subseteq> G" by simp
-
+  moreover have "?Yn \<subseteq> G"
+  by (metis Union_upper `\<Union>Range (a\<inverse>) = G` `{a\<inverse> ,,, n} \<subseteq> Range (a\<inverse>)` insert_subset)
+  moreover have "\<Union> (?r ?aa) \<subseteq> G"
+  by (metis Diff_subset Sup_subset_mono `Range (a\<inverse> -- n) = Range (a\<inverse>) - {a\<inverse> ,,, n}` `\<Union>Range (a\<inverse>) = G`)
+  then moreover have "?Y1 \<subseteq> G" 
+  by (metis Sup_le_iff `(a\<inverse> -- n) ,,, i \<in> Range (a\<inverse> -- n)`)
+  ultimately moreover have "?Y2 \<subseteq> G" by simp
   ultimately have 
   "(?aa +< (i, ?Y2))^-1 \<in> ?P G ((N-{n}) \<union> {i})" by (metis Un_absorb2)
   then have 
   "(?aa +< (i, ?Y2))^-1 \<in> ?P G (N-{n})" using 6 by force
   then have "?aa +< (i, ?Y2) \<in> ?X" by (metis converse_converse image_eqI)
   then have "?p b (?aa +< (i, ?Y2)) \<in> (?p b)`?X" by blast
-  moreover have "finite (?p b ` ?X)" sorry
+  moreover have "finite (?p b ` ?X)" using assms(3) by (metis finite_imageI)
   ultimately have "?p b (?aa +< (i, ?Y2)) \<le> Max ((?p b) ` ?X)" using Max_ge by blast
-  moreover have "b (i, ?Y1) \<le> b (i, ?Y2)" sorry (* MC: this is monotonicity assumption *)
-  moreover have "finite ?aa" sorry
+  moreover have "b (i, ?Y1) \<le> b (i, ?Y2)" using 7 0 by presburger
+  moreover have "finite ?aa" using assms(4) by (metis `a\<inverse> -- n \<subseteq> a\<inverse>` finite_converse finite_subset)
   ultimately moreover have " ?p b (?aa +< (i, ?Y2)) \<ge> ?p b (?aa +< (i, ?Y1))" using lll75
   by blast
   ultimately show ?thesis  using 1 by force
 qed
 
-lemma "value_rel (b::bids) a = proceeds (altbids b) (a^-1)" using value_rel_def sorry
+lemma "value_rel (b::bids) a = proceeds (altbids b) (a^-1)" using value_rel_def split_def 
+sorry
+
+corollary assumes "runiq (P^-1)" shows "Range (P outside X) \<inter> Range (P || X)={}"
+using assms lll78 Outside_def Range_def by (metis lll01 lll85)
 
 end
 
