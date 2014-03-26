6a9709adddfd4e8123d7022275ed4f6c740535ca
krbr <mnfrd.krbr@gmail.com>
2014-03-26 12:30:06 +0000

    scala code

---
+++ scala/AuctionMatrix.scala
@@ -0,0 +1,85 @@
+object AuctionMatrix {
+    type Bidvector = Array[Int];
+    type Bidmatrix = Array[Bidvector];
+    var n: Int = 3; // bidders
+    var t: Int = 10; // rounds = running time
+
+
+    //var bidmatrix = new scala.collection.mutable.ArrayBuffer[Array[Int]]()
+
+    var bidmatrix = Array.ofDim[Int](n,t);
+    
+    def localToInt(s: String):Int = {
+        // from http://alvinalexander.com/scala/how-cast-string-to-int-in-scala-string-int-conversion
+        try {
+            s.toInt
+        } catch {
+        case e:Exception => 0
+        }
+    }
+
+    def validBid(bid:Int,i:Int,j:Int) : Boolean = {
+        bid > bidmatrix{i}{j-1}
+    }
+
+    def roundOfBidding(j: Int) = {
+    for(i <- 0 to n-1) {
+        val x = readLine;
+        val newBid = localToInt(x);
+        if (validBid(newBid,i,j)) {
+            bidmatrix{i}{j} = newBid;
+        } else {
+            bidmatrix{i}{j} = bidmatrix{i}{j-1};
+        }
+    }
+    for(i <- 0 to n-1) {
+        for (k <- 0 to j) {
+            print(bidmatrix{i}{k} + " ")
+                }
+        println();
+    }
+}
+
+
+//    var bidmatrix = Array(1,2,3)
+//        bidmatrix = bidmatrix map(_*3)
+
+//    var j: Int = 1;
+    def main(args: Array[String]) {
+        var j: Int = 0;
+        while (life(bidmatrix, j)) {
+                j = j+1;
+                roundOfBidding(j);
+        }
+        winner(j);
+    }
+
+    def life(bidmatrix : Bidmatrix, j : Int) : Boolean = {
+    var result: Boolean = true;
+        if (j != 0) {
+            result = false;
+            var i : Int = 0
+            while (!result && i < n) {
+                if (bidmatrix{i}{j} > bidmatrix{i}{j-1}) {
+                    result = true;
+                } else {
+                    i = i+1;
+                }
+            }
+        }
+        result}
+
+    def winner(j : Int) = {
+            var maxbid : Int = bidmatrix{0}{j};
+            var result : Int = 0;
+        for (k <- 1 to n-1) {
+            if (bidmatrix{k}{j} > maxbid) {
+                maxbid = bidmatrix{k}{j}
+                result = k
+            }
+        }
+            println("The winner is: " + result)
+            println("The max bid is: " + maxbid)
+    }
+}
+//val ys = xs filter (x => x % 2 == 0)
