17db7e7fe1e462172f7f9c45802d22bb840980da
caminati <spam.caminati@yahoo.com>
2014-04-04 16:24:44 +0100

    corrected right-uniqueness (soundness)

--- isabelle/Auction/CombinatorialVickreyAuctionSoundness.thy
+++ isabelle/Auction/CombinatorialVickreyAuctionSoundness.thy
@@ -20,8 +20,12 @@ shows "paymentsRel N G t b n \<ge> 0 & paymentsRel N G t b n \<in> Reals" using 
 Reals_def by auto
 
 (*MC: right-uniqueness *)
-lemma "runiq (Graph (winningAllocationRel N G t b))" using l14b by fast
-lemma "runiq (Graph (paymentsRel N G t b))" using l14b by fast
+lemma "runiq (Graph (\<lambda> N. winningAllocationRel N G t b))" using l14b by fast
+lemma "runiq (Graph (\<lambda> G. winningAllocationRel N G t b))" using l14b by fast
+lemma "runiq (Graph (\<lambda> b. winningAllocationRel N G t b))" using l14b by fast
+lemma "runiq (Graph (\<lambda> N. paymentsRel N G t b))" using l14b by fast
+lemma "runiq (Graph (\<lambda> G. paymentsRel N G t b))" using l14b by fast
+lemma "runiq (Graph (\<lambda> b. paymentsRel N G t b))" using l14b by fast
 
 lemma (*MC: each good gets allocated exactly once, and no other thing gets allocated *)
 assumes "winningAllocationRel N G t b \<in> winningAllocationsRel N G b" shows
