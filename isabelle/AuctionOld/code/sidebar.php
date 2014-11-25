6ade32abf1caf238bfecf5a5da69aea5c911b5f9
caminati <spam.caminati@yahoo.com>
2014-11-25 15:43:26 +0000

    cleaned dir

--- isabelle/AuctionOld/code/sidebar.php
+++
@@ -1,4 +0,0 @@
-<li><a href="../..">Auction Theory Toolbox (ATT)</a></li>
-<li><a href="/research/projects/formare/">ForMaRE Project</a></li>
-<li><a href="http://isabelle.in.tum.de">Isabelle</a></li>
-<li><a href="http://scala-lang.org">Scala</a></li>
