27ec04a9530792b0d5c480006505de9b0278b018
krbr <mnfrd.krbr@gmail.com>
2015-03-02 18:14:30 +0000

    added some more comments

--- isabelle/Auction/Vcg/Argmax.thy
+++ isabelle/Auction/Vcg/Argmax.thy
@@ -22,6 +22,10 @@ imports Main
 
 begin
 
+text {* Structural induction is used in proofs on lists.*}
+lemma structInduct: assumes "P []" and "\<forall>x xs. P (xs) \<longrightarrow> P (x#xs)" shows "P l" 
+using assms list_nonempty_induct by (metis)
+
 text {* the subset of elements of a set where a function reaches its maximum *}
 fun argmax :: "('a \<Rightarrow> 'b\<Colon>linorder) \<Rightarrow> 'a set \<Rightarrow> 'a set"
 where "argmax f A = { x \<in> A . f x = Max (f ` A) }"
@@ -32,23 +36,18 @@ text {* The arg max of a function over a non-empty set is non-empty. *}
 corollary argmax_non_empty_iff: assumes "finite X" "X \<noteq> {}" shows "argmax f X \<noteq>{}"
 using assms Max_in finite_imageI image_is_empty lm79 lm86b by (metis(no_types))
 
-(* MC: Yet another approach below, focusing on reusing stuff (Max, zip, map, filter) 
-rather than doing our own recursion to calculate argmaxList *)
-
-text {* We want the elements of a list satisfying a given predicate;
+text {* The previous definition of argmax operates on sets. In the following we define a corresponding notion on lists. To this end, we start with defining a filter predicate and are looking for the elements of a list satisfying a given predicate;
 but, rather than returning them directly, we return the (sorted) list of their indices. 
 This is done, in different ways, by @{term filterpositions} and @{term filterpositions2}.*}
-definition filterpositions 
-(* MC: Non(directly)recursive generalization for max_positions, have no idea about efficiency.
-Given a list l, yields the indices of its elements which satisfy a given pred P*)
-:: "('a => bool) => 'a list => nat list"
+
+(* Given a list l, filterpositions yields the indices of its elements which satisfy a given pred P*)
+definition filterpositions :: "('a => bool) => 'a list => nat list"
 where "filterpositions P l = map snd (filter (P o fst) (zip l (upt 0 (size l))))"
+(* That is, you take the list [a0, a1, ..., an] pair with the indices [0, 1, ..., n], i.e., you get
+   [(a0,0), (a1,1), ..., (an,n)] look where the predicate (P o fst) holds and return the list of the corresponding snd elements. *)
 
-(* MC: After some experimentations, the following, more expressive equivalent
-writing also appears to be computable:*)
-
-definition filterpositions2 
-(* :: "('a => bool) => 'a list => nat list" *)
+(* Alternative definition, making use of list comprehension. In the next line the type info is commented out, since the type inference can be left to Isabelle. *)
+definition filterpositions2 (*  :: "('a => bool) => 'a list => nat list" *)
 where "filterpositions2 P l = [n. n \<leftarrow> [0..<size l], P (l!n)]"
 
 definition maxpositions (*:: "'a::linorder list => nat list"*) where
@@ -57,17 +56,16 @@ definition maxpositions (*:: "'a::linorder list => nat list"*) where
 lemma ll5: "maxpositions l = [n. n\<leftarrow>[0..<size l], l!n \<ge> Max(set l)]" 
 using assms unfolding maxpositions_def filterpositions2_def by fastforce
 
-definition argmaxList
-(*:: "('a => ('b::linorder)) => 'a list => 'a list"*)
-where "argmaxList f l = map (nth l) (maxpositions (map f l))"
+(* argmaxList takes a function and a list as arguments and looks for the positions of the elements at which the function applied to the list element is maximal, e.g., 
+for the list [9, 3, 5, 9, 13] and the function `modulo 8', the function applied to the list would give the list [1, 3, 5, 1, 5], that is, argmaxList will return [2, 4]. *)
+definition argmaxList (*:: "('a => ('b::linorder)) => 'a list => 'a list"*)
+ where "argmaxList f l = map (nth l) (maxpositions (map f l))"
 
-lemma "[n . n <- [0..<m], (n \<in> set [0..<m] & P n)] 
-= [n . n <- [0..<m], n \<in> set [0..<m], P n]" by meson
-
+(* The following lemmas state some relationships between different representation such as map and list comprehension *)
 lemma ll7b: "[n . n <- l, P n] = [n . n <- l, n \<in> set l, P n]" 
 proof - 
 (*MC: sledgehammer-generated proof. 
-I commented out the first three lines (they look quite useless), making it arguably readable. 
+I commented out the first three lines (they look quite useless), making it more readable. 
   assume "\<forall>v0. SMT2.fun_app uu__ v0 = (if P v0 then [v0] else [])"
   assume "\<forall>v0. SMT2.fun_app uua__ v0 = (if v0 \<in> set l then if P v0 then [v0] else [] else [])" 
   obtain v3_0 :: "('a \<Rightarrow> 'a list) \<Rightarrow> 'a list \<Rightarrow> ('a \<Rightarrow> 'a list) \<Rightarrow> 'a" where *) 
@@ -78,26 +76,26 @@ I commented out the first three lines (they look quite useless), making it argua
 qed
 
 lemma ll7: "[n . n <- [0..<m], P n] = [n . n <- [0..<m], n \<in> set [0..<m], P n]" using ll7b by fast
-(* concat_map_singleton map_ident  map_ext by smt*)
+(* sledgehammer suggested:  concat_map_singleton map_ident  map_ext by smt*)
 
 lemma ll10: fixes f m P shows "(map f [n . n <- [0..<m], P n]) = [ f n . n <- [0..<m], P n]" 
 by (induct m) auto
 
+(* Base case stating the property for the empty list *)
 lemma map_commutes_a: "[f n . n <- [], Q (f n)] = [x <- (map f []). Q x]" by simp
 
+(* Step case where the element x is added to the list xs *)
 lemma map_commutes_b: "\<forall> x xs. ([f n . n <- xs, Q (f n)] = [x <- (map f xs). Q x]
 \<longrightarrow> [f n . n <- (x#xs), Q (f n)] = [x <- (map f (x#xs)). Q x])" using assms by simp
 
-lemma structInduct: assumes "P []" and "\<forall>x xs. P (xs) \<longrightarrow> P (x#xs)" shows "P l" 
-using assms list_nonempty_induct by (metis)
-
-(* MC: map_commutes, filterpositions are fairly general and should be moved elsewhere *)
+(* General case comprising the two previous cases. *)
 lemma map_commutes: fixes f::"'a => 'b" fixes Q::"'b => bool" fixes xs::"'a list" 
 shows "[f n . n <- xs, Q (f n)] = [x <- (map f xs). Q x]"
 using map_commutes_a map_commutes_b structInduct by fast
 
-lemma ll9: fixes f l shows "maxpositions (map f l) =
-[n . n <- [0..<size l], f (l!n) \<ge> Max (f`(set l))]" (is "maxpositions (?fl) = _")
+lemma ll9: fixes f l shows "maxpositions (map f l) = 
+              [n . n <- [0..<size l], f (l!n) \<ge> Max (f`(set l))]" 
+(is "maxpositions (?fl) = _") (* Pattern matching abbreviation ?fl corresponds to (map f l). Used in the proof, not part of lemma itself *)
 proof -
   have "maxpositions ?fl = 
   [n. n <- [0..<size ?fl], n\<in> set[0..<size ?fl], ?fl!n \<ge> Max (set ?fl)]"
@@ -114,15 +112,15 @@ proof -
   finally show ?thesis by auto
 qed
 
-lemma ll11: fixes f l shows "argmaxList f l = [ l!n . n <- [0..<size l], f (l!n) \<ge> Max (f`(set l))]"
+lemma ll11: fixes f l shows "argmaxList f l = 
+               [ l!n . n <- [0..<size l], f (l!n) \<ge> Max (f`(set l))]"
 unfolding ll9 argmaxList_def by (metis ll10)
 
-text{* RHS of thesis should formally reassure about what @{term argmaxList} does.
-It was not directly used as a definition of the latter (both appear 
-to be computable) because @{term filterpositions2} has independent, more general interest. *}
+text{* The theorem expresses that argmaxList is the list of arguments greater equal the Max of the list.  *}
 theorem argmaxadequacy: 
 fixes f::"'a => ('b::linorder)" fixes l::"'a list" shows 
-"argmaxList f l = [ x <- l. f x \<ge> Max (f`(set l))]" (is "?lh=_")
+"argmaxList f l = [ x <- l. f x \<ge> Max (f`(set l))]"
+(is "?lh=_") (* pattern match ?lh abbreviates "argmaxList f l" *)
 proof -
   let ?P="% y::('b::linorder) . y \<ge> Max (f`(set l))"
   let ?mh="[nth l n . n <- [0..<size l], ?P (f (nth l n))]"
