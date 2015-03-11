336875f455cdea91085ee165e9dde04ebe935a5e
krbr <mnfrd.krbr@gmail.com>
2015-03-11 17:30:49 +0000

    partitions changed

--- isabelle/Auction/Vcg/RelationOperators.thy
+++ isabelle/Auction/Vcg/RelationOperators.thy
@@ -3,7 +3,9 @@ Auction Theory Toolbox (http://formare.github.io/auctions/)
 
 Authors:
 * Marco B. Caminati http://caminati.co.nr
+* Manfred Kerber <mnfrd.krbr@gmail.com>
 * Christoph Lange <math.semantic.web@gmail.com>
+* Colin Rowat <c.rowat@bham.ac.uk>
 
 Dually licenced under
 * Creative Commons Attribution (CC-BY) 3.0
@@ -11,6 +13,7 @@ Dually licenced under
 See LICENSE file for details
 (Rationale for this dual licence: http://arxiv.org/abs/1107.3212)
 *)
+
 
 header {* Additional operators on relations, going beyond Relations.thy,
   and properties of these operators *}
@@ -40,18 +43,12 @@ section {* restriction *}
 
 text {* restriction of a relation to a set (usually resulting in a relation with a smaller domain) *}
 definition restrict
-(* TODO MC: compare with restr in SchorrWaite.thy
-   CL@MC: doesn't seem helpful, as its type "('a \<times> 'a) set \<Rightarrow> ('a \<Rightarrow> bool) \<Rightarrow> ('a \<times> 'a) set" is 
-   more specific than what we need. *)
 :: "('a \<times> 'b) set \<Rightarrow> 'a set \<Rightarrow> ('a \<times> 'b) set" (infix "||" 75)
-where "R || X = X \<times> Range R \<inter> R"
+where "R || X = (X \<times> Range R) \<inter> R"
 
-text {* extensional characterisation of the pairs within a restricted relation *}
+text {* extensional characterization of the pairs within a restricted relation *}
 lemma restrict_ext: "R || X = {(x, y) | x y . x \<in> X \<and> (x, y) \<in> R}"
 unfolding restrict_def using Range_iff by blast
-(* CL: This proof seems impossible for sledgehammer.  Range_iff is not a simp rule.  I managed
-   to arrive at this point after painfully rewriting the set comprehension in very small steps,
-   only to see that most of these steps could be proved by blast. *)
 
 text {* alternative statement of @{thm restrict_ext} without explicitly naming the pair's components *}
 lemma restrict_ext': "R || X = {p . fst p \<in> X \<and> p \<in> R}"
@@ -70,8 +67,8 @@ lemma restriction_is_subrel: "P || X \<subseteq> P" using restrict_def by blast
 text {* Restricting a relation only has an effect within its domain. *}
 lemma restriction_within_domain: "P || X = P || (X \<inter> (Domain P))" unfolding restrict_def by fast
 
-text {* alternative characterisation of the restriction of a relation to a singleton set *}
-lemma restrict_to_singleton: "P || {x} = {x} \<times> P `` {x}" unfolding restrict_def by fast
+text {* alternative characterization of the restriction of a relation to a singleton set *}
+lemma restrict_to_singleton: "P || {x} = {x} \<times> (P `` {x})" unfolding restrict_def by fast
 
 section {* relation outside some set *}
 
@@ -82,7 +79,7 @@ definition Outside :: "('a \<times> 'b) set \<Rightarrow> 'a set \<Rightarrow> (
 where "R outside X = R - (X \<times> Range R)"
 
 text {* Considering a relation outside some set @{term X} reduces its domain by @{term X}. *}
-lemma outside_reduces_domain: "Domain (P outside X) = Domain P - X"
+lemma outside_reduces_domain: "Domain (P outside X) = (Domain P) - X"
 unfolding Outside_def by fast
 
 text {* Considering a relation outside a singleton set @{term "{x}"} reduces its domain by 
@@ -97,7 +94,7 @@ by (metis Diff_insert_absorb)
 
 text {* For any set, a relation equals the union of its restriction to that set and its
   pairs outside that set. *}
-lemma outside_union_restrict: "P = P outside X \<union> P || X"
+lemma outside_union_restrict: "P = (P outside X) \<union> (P || X)"
 unfolding Outside_def restrict_def by fast
 
 text {* The range of a relation @{term R} outside some exclusion set @{term X} is a 
@@ -105,7 +102,7 @@ text {* The range of a relation @{term R} outside some exclusion set @{term X} i
 lemma Range_outside_sub_Image_Domain: "Range (R outside X) \<subseteq> R `` (Domain R - X)"
 using Outside_def Image_def Domain_def Range_def by blast
 
-text {* Considering a relation outside some set doesn't enlarge its range. *}
+text {* Considering a relation outside some set does not enlarge its range. *}
 lemma Range_outside_sub:
   assumes "Range R \<subseteq> Y"
   shows "Range (R outside X) \<subseteq> Y"
@@ -140,7 +137,7 @@ qed
 section {* evaluation as a function *}
 
 text {* Evaluates a relation @{term R} for a single argument, as if it were a function.
-  This will only work if @{term R} is a total function, i.e. if the image is always a singleton set. *}
+  This will only work if @{term R} is right-unique, i.e. if the image is always a singleton set. *}
 fun eval_rel :: "('a \<times> 'b) set \<Rightarrow> 'a \<Rightarrow> 'b" (infix ",," 75) (* . (Mizar's notation) confuses Isar *)
 where "R ,, a = the_elem (R `` {a})"
 
@@ -181,6 +178,7 @@ lemma paste_sub_Un: "P +* Q \<subseteq> P \<union> Q" unfolding paste_def Outsid
 
 text {* The range of two pasted relations is a subset of the union of their ranges. *}
 lemma paste_Range: "Range (P +* Q) \<subseteq> Range P \<union> Range Q"
+
 (* TODO CL: report bug that structured proof found by auto sledgehammer doesn't work *)
 using paste_sub_Un by blast
 
